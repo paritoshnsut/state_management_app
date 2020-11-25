182e64a5834c1038fae2ab760da970c3cbd8fae4
Paritosh <kpari307@gmail.com>
2020-11-25 22:30:47 +0530

    Made some edits

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-         appBar: AppBar(
-           elevation: 0.0,
-           title: Text("Restrovaganza" , style: TextStyle(color: Colors.black),),
-           leading: IconButton(icon: Icon(Icons.menu), color: Colors.black,onPressed: (){},),
-           
-         ),
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text(
+            "Restrovaganza",
+            style: TextStyle(color: Colors.black),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            color: Colors.black,
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            )
+          ],
+        ),
       ),
     );
   }
